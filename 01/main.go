fe96bba42b093782eab546df92d90106f505d737
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2021-12-08 21:38:30 -0600

    Primer ejercicio segunda parte

--- 01/main.go
+++ 01/main.go
@@ -14,27 +14,55 @@ func main() {
 		fmt.Errorf("error al leer el archivo: %s", err)
 	}
 
-	numbers := strings.Split(string(textInFile), "\n")
+	numbers := []int{}
 
-	increased := 0
-	previews, err := strconv.Atoi(numbers[0])
-	if err != nil {
-		fmt.Errorf("error al convertir el numero preview %s", err)
-	}
-	next := numbers[1:]
-
-	for _, n := range next {
-		nextNumber, err := strconv.Atoi(n)
+	text := strings.TrimSpace(string(textInFile))
+	textList := strings.Split(text, "\n")
+	for _, n := range textList {
+		number, err := strconv.Atoi(n)
 		if err != nil {
-			fmt.Errorf("error al convertir el numero preview %s", err)
+			panic(err)
 		}
 
+		numbers = append(numbers, number)
+	}
+
+	part1(numbers)
+	part2(numbers)
+}
+
+func part2(previewsList []int) {
+
+	increased := 0
+
+	for i := range previewsList {
+		if i+4 > len(previewsList) {
+			break
+		}
+
+		primeraSuma := previewsList[0+i] + previewsList[1+i] + previewsList[2+i]
+		segundaSuma := previewsList[1+i] + previewsList[2+i] + previewsList[3+i]
+
+		if segundaSuma > primeraSuma {
+			increased++
+		}
+	}
+
+	fmt.Println(increased)
+}
+
+func part1(numbers []int) {
+
+	increased := 0
+	previews := numbers[0]
+
+	next := numbers[1:]
+
+	for _, nextNumber := range next {
 		if nextNumber > previews {
 			increased++
 		}
 		previews = nextNumber
 	}
-
 	fmt.Println(increased)
-
 }
